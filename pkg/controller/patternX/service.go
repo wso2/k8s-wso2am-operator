fd4ab9d7153da1b4d219615e2805c5ff1d3c6e53
BLasan <benuraab@gmail.com>
2021-01-15 18:35:38 +0530

    expose patternx using ingress add scenario-18

--- pkg/controller/patternX/service.go
+++ pkg/controller/patternX/service.go
@@ -21,31 +21,31 @@
 package patternX
 
 import (
+	apimv1alpha1 "github.com/wso2/k8s-wso2am-operator/pkg/apis/apim/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	apimv1alpha1 "github.com/wso2/k8s-wso2am-operator/pkg/apis/apim/v1alpha1"
 )
 
 // newService creates a new Service for a Apimanager resource.
 // It expose the service with Nodeport type with minikube ip as the externel ip.
-func ApimXService(apimanager *apimv1alpha1.APIManager,r *apimv1alpha1.Profile) *corev1.Service {
+func ApimXService(apimanager *apimv1alpha1.APIManager, r *apimv1alpha1.Profile) *corev1.Service {
 	labels := map[string]string{
 		"deployment": r.Name,
 	}
 
 	apimcommonsvsports := []corev1.ServicePort{}
 	servType := ""
-	if apimanager.Spec.Service.Type == "NodePort"{
+	if apimanager.Spec.Service.Type == "NodePort" {
 		apimcommonsvsports = getApimCommonSvcNPPorts()
 		servType = "NodePort"
-	}else if apimanager.Spec.Service.Type == "LoadBalancer"{
+	} else if apimanager.Spec.Service.Type == "LoadBalancer" {
 		apimcommonsvsports = getApimCommonSvcPorts()
 		servType = "LoadBalancer"
-	}else if apimanager.Spec.Service.Type == "ClusterIP"{
+	} else if apimanager.Spec.Service.Type == "ClusterIP" {
 		apimcommonsvsports = getApimCommonSvcPorts()
 		servType = "ClusterIP"
-	} else{
+	} else {
 		apimcommonsvsports = getApimCommonSvcPorts()
 		servType = "LoadBalancer"
 	}
@@ -60,23 +60,23 @@ func ApimXService(apimanager *apimv1alpha1.APIManager,r *apimv1alpha1.Profile) *
 		},
 		Spec: corev1.ServiceSpec{
 			Selector: labels,
-			Type:    corev1.ServiceType(servType),
-			Ports: apimcommonsvsports,
+			Type:     corev1.ServiceType(servType),
+			Ports:    apimcommonsvsports,
 		},
 	}
 }
 
-func DashboardXService(apimanager *apimv1alpha1.APIManager,r *apimv1alpha1.Profile) *corev1.Service {
+func DashboardXService(apimanager *apimv1alpha1.APIManager, r *apimv1alpha1.Profile) *corev1.Service {
 	labels := map[string]string{
 		"deployment": r.Name,
 	}
 
 	dashBoardPorts := []corev1.ServicePort{}
-	servType :=""
-	if apimanager.Spec.Service.Type == "NodePort"{
+	servType := ""
+	if apimanager.Spec.Service.Type == "NodePort" {
 		dashBoardPorts = getDashBoardNPPorts()
 		servType = "NodePort"
-	} else if apimanager.Spec.Service.Type == "LoadBalancer"{
+	} else if apimanager.Spec.Service.Type == "LoadBalancer" {
 		dashBoardPorts = getDashBoardPorts()
 		servType = "LoadBalancer"
 	} else if apimanager.Spec.Service.Type == "ClusterIP" {
@@ -97,13 +97,13 @@ func DashboardXService(apimanager *apimv1alpha1.APIManager,r *apimv1alpha1.Profi
 		},
 		Spec: corev1.ServiceSpec{
 			Selector: labels,
-			Type:    corev1.ServiceType(servType),
-			Ports: dashBoardPorts,
+			Type:     corev1.ServiceType(servType),
+			Ports:    dashBoardPorts,
 		},
 	}
 }
 
-func WorkerXService(apimanager *apimv1alpha1.APIManager,r *apimv1alpha1.Profile) *corev1.Service {
+func WorkerXService(apimanager *apimv1alpha1.APIManager, r *apimv1alpha1.Profile) *corev1.Service {
 	labels := map[string]string{
 		"deployment": r.Name,
 	}
@@ -112,7 +112,7 @@ func WorkerXService(apimanager *apimv1alpha1.APIManager,r *apimv1alpha1.Profile)
 
 	return &corev1.Service{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:     r.Service.Name,
+			Name:      r.Service.Name,
 			Namespace: apimanager.Namespace,
 			OwnerReferences: []metav1.OwnerReference{
 				*metav1.NewControllerRef(apimanager, apimv1alpha1.SchemeGroupVersion.WithKind("APIManager")),
@@ -127,42 +127,36 @@ func WorkerXService(apimanager *apimv1alpha1.APIManager,r *apimv1alpha1.Profile)
 					Protocol:   corev1.ProtocolTCP,
 					Port:       7612,
 					TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 7612},
-
 				},
 				{
 					Name:       "thrift-ssl",
 					Protocol:   corev1.ProtocolTCP,
 					Port:       7712,
 					TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 7712},
-
 				},
 				{
 					Name:       "rest-api-port-1",
 					Protocol:   corev1.ProtocolTCP,
 					Port:       9444,
 					TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 9444},
-
 				},
 				{
 					Name:       "rest-api-port-2",
 					Protocol:   corev1.ProtocolTCP,
 					Port:       9091,
 					TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 9091},
-
 				},
 				{
 					Name:       "rest-api-port-3",
 					Protocol:   corev1.ProtocolTCP,
 					Port:       7071,
 					TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 7071},
-
 				},
 				{
 					Name:       "rest-api-port-4",
 					Protocol:   corev1.ProtocolTCP,
 					Port:       7444,
 					TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: 7444},
-
 				},
 			},
 		},
--- scenarios/scenario-18/README.md
+++ scenarios/scenario-18/README.md
@@ -1,19 +1,92 @@
-## Scenario-18 : Override ConfigMaps
+## Scenario-18 : Expose API Manager using Ingress (Custom Pattern)
 
-You can override the default deployemnt configmaps present for each profile.
+In this scenario we are exposing a single API Manager instance in Kubernetes using Ingress.
 
-1. Create a new deployment configmap using the deployment.toml/ deployment.yaml files. And apply the command.
+#### Deploy the configmap and the custom pattern
 
+The configmap contains the configurations of API Manager node. 
+
+```
+ kubectl create -f wso2am-apim-configmap.yaml
+ kubectl create -f custom-pattern.yaml
+```
+
+#### Access API Manager
+
+To access the API Manager, consider the following steps.
+
+  1. Get the external address of the ingresses using the command,
     ```
-      kubectl create configmap <NEW-PROFILE-CONFIGMAP> --from-file=<PATH_TO_DEPLOYMENT.TOML>
+    kubectl get ingress
+    
+    Output:
+    NAME                                     HOSTS                ADDRESS           PORTS     AGE
+    wso2-am-gateway-px-ingress               wso2apim-gateway     104.154.172.7     80, 443   3m18s
+    wso2-am-px-ingress                       wso2apim             104.154.172.7     80, 443   3m18s
     ```
-
-2. Update the custom resources with created configmap.
-
-    Replace, the **deploymentConfigmap** with configmap name.
-
-3. Deploy updated pattern-4.
-
+        
+  2. Add an **/etc/hosts/** entry as follows.
+        
     ```
-      kubectl apply -f wso2-apim.yaml
-    ```
+    /etc/hosts
+    ----------
+    <EXTERNAL-ADDRESS>       wso2apim wso2apim-gateway wso2apim-analytics    
+  
+- **API Publisher** : https://wso2apim/publisher 
+- **API Devportal** : https://wso2apim/devportal 
+
+
+## Custom Patterns
+
+You can introduce a new custom pattern with any number of profiles.
+
+There are several types of profiles:
+
+    * api-manager
+    * analytics-dashboard
+    * analytics-worker
+
+
+- You can create any no.of profiles with given types.
+- You can provide desired configuration values if you wish, if not provided it will take upon the default type specific configuration values.
+- Under a profile, everything except name, type, service, and configMaps are optional fields.
+
+#### Available options in a custom pattern can be found here.
+
+```
+apiVersion: apim.wso2.com/v1alpha1
+kind: APIManager
+metadata:
+  name: cluster-1
+spec:
+  pattern: Pattern-X
+  service:
+    type: NodePort
+  useMysql: "<true|false>"
+  profiles:
+    - name: <NEW_PROFILE_NAME>
+      type: api-manager
+      service:
+        name: <NEW_PROFILE_SERVICE_NAME>
+      deployment:
+        replicas: 1
+        minReadySeconds: 100
+        imagePullPolicy: Always
+        resources:
+          requests:
+            memory: 2Gi
+            cpu: 2000m
+          limits:
+            memory: 3Gi
+            cpu: 3000m
+        livenessProbe:
+          initialDelaySeconds: 240
+          failureThreshold: 3
+          periodSeconds: 10
+        readinessProbe:
+          initialDelaySeconds: 240
+          failureThreshold: 3
+          periodSeconds: 10
+        configMaps:
+          deploymentConfigMap: <NEW_PROFILE_DEPLOYMENT_CONFIGMAP>
+```
